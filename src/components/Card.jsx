0bc2582fd8abbf5ef92c3a34f5d6736e8763ec3b
tanzim-nahid <tanzimnahid6@gmail.com>
2023-03-30 11:37:16 +0600

    show all button add,using boolean

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,15 +3,19 @@ import SingleCard from './SingleCard';
 
 const Card = () => {
     const [products,setProducts] = useState([])
+    const [showAll,setShowAll] = useState(false)
     useEffect(()=>{
         fetch('product.json')
             .then(res=>res.json())
             .then(data=>setProducts(data))
     },[])
     return (
-        <div className='grid grid-cols-3 p-8 gap-5'>
+        <div className='grid grid-cols-3 p-8  gap-5'>
             {
-                products.map((product)=><SingleCard key={product.id} product={product}></SingleCard>)
+                products.slice(0,showAll ? 6 : 3).map((product)=><SingleCard key={product.id} product={product}></SingleCard>)
+            }
+            {
+                !showAll && <button onClick={()=>setShowAll(true)} className='btn btn-primary text-center'>Show ALL</button>
             }
         </div>
     );
